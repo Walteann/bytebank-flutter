6a329b5236e528c87c3c9aec91871bf6add20ab8
Walteann <walteann3@gmail.com>
2025-10-28 20:32:03 -0300

    feat: estrutura de pastas

---
+++ lib/ui/themes/app-colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6200EE); // Exemplo de cor principal
+  static const Color secondaryColor = Color(0xFF03DAC5); // Exemplo de cor secundária
+  static const Color backgroundColor = Color(0xFFF5F5F5); // Cor de fundo
+  static const Color textColor = Color(0xFF000000); // Cor do texto
+  static const Color accentColor = Color(0xFFFF5722); // Cor de destaque
+
+  // Adicione mais cores conforme necessário
+}
